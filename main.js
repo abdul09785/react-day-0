d33c7379b9a22387de37f5462adec02c3efa14c9
unknown <abdulnoman608@gmail.com>
2026-07-02 23:50:12 +0530

    First React project

---
+++ main.js
@@ -0,0 +1,46 @@
+﻿// const heading = React.createElement("h1", {}, "Hello React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// import React, { useState } from "react";
+
+// function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   function increase() {
+//     setCount(count + 1);
+//   }
+
+//   function decrease() {
+//     setCount(count - 1);
+//   }
+
+//   function reset() {
+//     setCount(0);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Counter App</h1>
+//       <h2>{count}</h2>
+
+//       <button onClick={increase}>+</button>
+//       <button onClick={decrease}>-</button>
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
+
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {}, "Hello React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
